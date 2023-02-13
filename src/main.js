6069cca5c2c09f9c684c7194d2cf8972b0ab1d3e
Daniel Bianchin <danielbianchin@outlook.com.br>
2023-02-13 17:07:03 -0300

    adicionado copy e Build do VITE

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import { nanoid } from 'nanoid';
+import copy from 'clipboard-copy';
 import './style.css';
 
 const buttonElement = document.querySelector('button');
 
 const displayPassword = document.getElementsByTagName('h2')[0];
 
-/* addEventListener(buttonElement) = */
-
 buttonElement.addEventListener('click', () => {
   displayPassword.innerHTML = nanoid();
+});
+
+const h2 = document.querySelector('h2');
+
+h2.addEventListener('click', (element) => {
+  copy(element.target.innerHTML);
 });
